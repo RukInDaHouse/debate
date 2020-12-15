632edb13a6c4f922630c3b4f1e92442c465d28bc
RukInDaHouse <r1997p@mail.ru>
2020-12-15 20:34:16 +0300

    script

--- js/script.js
+++ js/script.js
@@ -1,72 +1,59 @@
-function change_color(elem, color) {
-elem.parentElement.style.background = color;
-}
-
-var addAudit = function() {
+var addWhiteBlock = function() {
 	var col3 = document.querySelector('.col-3');
 	var elemKod2 = document.querySelector('.in-kod2').innerHTML;
 
 	col3.innerHTML += '<div>' + elemKod2 + '</div>';
 };
-var addAudit2 = function() {
-	var col2 = document.querySelector('.col-2');
+
+var addBlueBlock = function() {
+	var col3 = document.querySelector('.col-3');
 	var elemKod3 = document.querySelector('.in-kod3').innerHTML;
 
-	col2.innerHTML += '<div>' + elemKod3 + '</div>';
+	col3.innerHTML += '<div>' + elemKod3 + '</div>';
 };
 
-var addAudit3 = function() {
-	var col1 = document.querySelector('.col-1');
-	var elemKod2 = document.querySelector('.in-kod2').innerHTML;
+var addPurpleBlock = function() {
+	var col6 = document.querySelector('.col-8');
+	var elemKod4 = document.querySelector('.in-kod4').innerHTML;
 
-	col1.innerHTML += '<div>' + elemKod2 + '</div>';
+	col6.innerHTML += '<div>' + elemKod4 + '</div>';
 };
 
-
-
-var addAudit5 = function() {
-	var col7 = document.querySelector('.col-7');
-	var elemKod5 = document.querySelector('.in-kod5').innerHTML;
-
-	col7.innerHTML += '<div>' + elemKod5 + '</div>';
-};
-
-var addAudit6 = function() {
-	var col8 = document.querySelector('.col-8');
-	var elemKod6 = document.querySelector('.in-kod6').innerHTML;
-
-	col8.innerHTML += '<div>' + elemKod6 + '</div>';
-};
-
-var addAudit7 = function() {
-	var col9 = document.querySelector('.col-9');
-	var elemKod7 = document.querySelector('.in-kod7').innerHTML;
-
-	col9.innerHTML += '<div>' + elemKod7 + '</div>';
-};
 
 document.onclick = function() {
 	var elem = window.event.srcElement;
 	if (elem.className === "btn") {
-		addAudit();
+		addWhiteBlock();
 	}
 	else if (elem.className === "btn2") {
-		addAudit2();
+		addBlueBlock();
 	}		
 	else if (elem.className === "btn3") {
-		addAudit3();
-	}
-	else if (elem.className === "btn4") {
-		addAudit4();
+		addPurpleBlock();
 	}		
-	else if (elem.className === "btn5") {
-		addAudit5();
-	}
-	else if (elem.className === "btn6") {
-		addAudit6();
-	}	
-	else if (elem.className === "btn7") {
-		addAudit7();
-	}			
 };
 
+function removeElem(delElem, attribute, attributeName) {
+  if (!(delElem && attribute && attributeName)) return;
+  return function(e) {
+    let target = e.target;
+    if (!(target.hasAttribute(attribute) ?
+        (target.getAttribute(attribute) === attributeName ? true : false) : false)) return;
+    let elem = target;
+    while (target != this) {
+      if (target.classList.contains(delElem)) {
+        target.remove();
+        return;
+      }
+      target = target.parentNode;
+    }
+    return;
+  };
+}
+
+
+document.addEventListener("click", removeElem("card", "data-del", "delete"));
+
+function change_color(elem, color) {
+	elem.parentElement.style.background = color;
+}
